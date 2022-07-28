d5316179ee374581822afaa669487697ae135fad
bgebelein <bent.gebelein@akquinet.de>
2022-07-28 09:43:49 +0200

    rounded buttons, added initial value for torch to getUserMedia constraints

--- instaminsta/script.js
+++ instaminsta/script.js
@@ -14,6 +14,9 @@ function initiateCamera () {
             width: {ideal: 4096},
             height: {ideal: 4096},
             facingMode: camera,
+            advanced: [{
+                torch: torch,
+            }]
         }
     })
     .then(function(mediaStream) {
